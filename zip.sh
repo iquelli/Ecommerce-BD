1e32fc580670139e532f88b751e675cc85065d3e
Gocho1234 <goncalo.barias@gmail.com>
2023-05-17 22:44:07 +0100

    Setup structure for project 2

---
+++ zip.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env sh
+
+GROUP=02
+FOLDER_NAME=entrega-02-$GROUP
+
+mkdir /tmp/$FOLDER_NAME
+cp ./reports/report2.pdf /tmp/$FOLDER_NAME/report.pdf
+cp -r ./scripts/*.sql /tmp/$FOLDER_NAME/
+cp ./output.txt /tmp/$FOLDER_NAME/
+
+pushd /tmp
+zip -r $FOLDER_NAME.zip $FOLDER_NAME
+popd
+
+mv /tmp/$FOLDER_NAME.zip .
+rm -r /tmp/$FOLDER_NAME
