141e22530edf463f8604fee18ac8b4b3008265e5
Gocho1234 <goncalo.barias@gmail.com>
2023-05-19 11:34:57 +0100

    Setup template for inserting values into tables

--- scripts/populate.sql
+++ scripts/populate.sql
@@ -6,3 +6,93 @@
 --              - Vasco Paisana (ist1102533)
 --		Group: 2
 --		Description: DML that populates the tables with synthetic test data.
+
+---------------------
+-- Customer
+---------------------
+INSERT INTO customer
+VALUES;
+
+---------------------
+-- Order
+---------------------
+INSERT INTO order
+VALUES;
+
+---------------------
+-- Sale
+---------------------
+INSERT INTO sale
+VALUES;
+
+---------------------
+-- Product
+---------------------
+INSERT INTO product
+VALUES;
+
+---------------------
+-- Ean Product
+---------------------
+INSERT INTO ean_product
+VALUES;
+
+---------------------
+-- Contains
+---------------------
+INSERT INTO contains
+VALUES;
+
+---------------------
+-- Supplier
+---------------------
+INSERT INTO supplier
+VALUES;
+
+---------------------
+-- Department
+---------------------
+INSERT INTO department
+VALUES;
+
+---------------------
+-- Workplace
+---------------------
+INSERT INTO workplace
+VALUES;
+
+---------------------
+-- Warehouse
+---------------------
+INSERT INTO warehouse
+VALUES;
+
+---------------------
+-- Delivery
+---------------------
+INSERT INTO delivery
+VALUES;
+
+---------------------
+-- Office
+---------------------
+INSERT INTO office
+VALUES;
+
+---------------------
+-- Employee
+---------------------
+INSERT INTO employee
+VALUES;
+
+---------------------
+-- Works
+---------------------
+INSERT INTO works
+VALUES;
+
+---------------------
+-- Process
+---------------------
+INSERT INTO process
+VALUES;
