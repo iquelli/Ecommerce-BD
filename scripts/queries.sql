1cdd1a28ea340092196aff041ac394631f6c9e76
Gocho1234 <goncalo.barias@gmail.com>
2023-06-05 22:51:38 +0100

    Add query 2 using division in sql

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -8,29 +8,40 @@
 --		Description: Presents the corresponding SQL expression for each
 --                   question proposed in section 2.
 
--- (1): List the name(s) of the client(s) with the biggest value of paid orders.
-
-SELECT customer.name
+-- (1): What is the number and name of the client(s) with the biggest value of paid orders?
+SELECT cust_no, customer.name
 FROM customer
-NATURAL JOIN pay
-GROUP BY customer.cust_no
-HAVING COUNT(pay.cust_no) = (
-    SELECT COUNT(pay.cust_no) AS order_count
+    NATURAL JOIN pay
+    NATURAL JOIN contains
+    JOIN product ON (contains.sku = product.sku)
+GROUP BY cust_no
+HAVING SUM(qty * price) >= ALL(
+    SELECT SUM(qty * price)
     FROM pay
-    GROUP BY pay.cust_no
-    ORDER BY order_count DESC
-    LIMIT 1
+        NATURAL JOIN contains
+        NATURAL JOIN product
+    GROUP BY cust_no
 );
 
--- (2): List the names of all the employees that processed packages everyday of 2022 in which there was a delivery.
-SELECT employee.name
-FROM employee;
--- ??????????????????????????
+-- (2): What are the names of all the employees that processed packages everyday of 2022 in which there was an order?
+SELECT DISTINCT name
+FROM employee
+WHERE NOT EXISTS (
+    SELECT date
+    FROM orders
+    WHERE EXTRACT(YEAR FROM date) = 2022
+    EXCEPT
+    SELECT date
+    FROM orders
+        NATURAL JOIN process
+    WHERE process.ssn = employee.ssn
+);
 
--- (3): List the amount of packages that were processed but not paid in each month of 2022.
-SELECT EXTRACT(MONTH FROM orders.date) AS month, COUNT(*) AS "unpaid orders"
+-- (3): How many orders were placed but not paid for in each month of 2022?
+SELECT EXTRACT(MONTH FROM date) AS month_2022, COUNT(*) AS processed_unpaid_orders
 FROM orders
-LEFT JOIN pay ON orders.order_no = pay.order_no
-WHERE EXTRACT(YEAR FROM orders.date) = 2022 AND pay.order_no IS NULL
-GROUP BY EXTRACT(MONTH FROM orders.date)
-ORDER BY month ASC;
+    LEFT JOIN pay ON (orders.order_no = pay.order_no)
+WHERE EXTRACT(YEAR FROM date) = 2022
+    AND pay.order_no IS NULL
+GROUP BY EXTRACT(MONTH FROM date)
+ORDER BY month_2022 ASC;
