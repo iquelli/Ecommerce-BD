2372030e6f1643377319e82bf8c36aba9b9ae575
Gocho1234 <goncalo.barias@gmail.com>
2023-06-05 18:05:55 +0100

    Clean up the scripts folder for E3

---
+++ scripts/run_scripts.sql
@@ -0,0 +1,15 @@
+--
+--		File: run_scripts.sql
+--		Authors:
+--		        - Gonçalo Bárias (ist1103124)
+--		        - Raquel Braunschweig (ist1102624)
+--              - Vasco Paisana (ist1102533)
+--		Group: 2
+--		Description: Runs all the scripts.
+
+\i schema.sql
+\i ICs.sql
+\i populate.sql
+\i view.sql
+\i queries.sql
+\i analytics.sql
--- scripts/schema.sql
+++ scripts/schema.sql
@@ -30,124 +30,96 @@ DROP TABLE IF EXISTS delivery CASCADE;
 -- and some of them are presented in ICs.sql.
 
 CREATE TABLE customer (
-    cust_no INT,
+    cust_no INT PRIMARY KEY,
     name VARCHAR(80) NOT NULL,
     email VARCHAR(254) NOT NULL UNIQUE,
     phone VARCHAR(15),
     address VARCHAR(255),
-    CONSTRAINT pk_customer PRIMARY KEY(cust_no),
     CHECK (email LIKE '_%@_%\._%')
 );
+
 CREATE TABLE orders (
-    order_no INT,
-    cust_no INT NOT NULL,
-    date DATE NOT NULL,
-    CONSTRAINT pk_orders PRIMARY KEY(order_no),
-    CONSTRAINT fk_orders_customer FOREIGN KEY(cust_no)
-        REFERENCES customer(cust_no)
+    order_no INT PRIMARY KEY,
+    cust_no INT NOT NULL REFERENCES customer,
+    date DATE NOT NULL
     -- order_no must exist in contains
 );
+
 CREATE TABLE pay (
-    order_no INT,
-    cust_no INT NOT NULL,
-    CONSTRAINT pk_pay PRIMARY KEY(order_no),
-    CONSTRAINT fk_pay_orders FOREIGN KEY(order_no)
-        REFERENCES orders(order_no),
-    CONSTRAINT fk_pay_customer FOREIGN KEY(cust_no)
-        REFERENCES customer(cust_no)
+    order_no INT PRIMARY KEY REFERENCES orders,
+    cust_no INT NOT NULL REFERENCES customer
 );
+
 CREATE TABLE employee (
-    ssn VARCHAR(20),
+    ssn VARCHAR(20) PRIMARY KEY,
     tin VARCHAR(20) NOT NULL UNIQUE,
     bdate DATE,
     name VARCHAR NOT NULL,
-    CONSTRAINT pk_employee PRIMARY KEY(ssn),
     CHECK (EXTRACT(YEAR FROM AGE(bdate)) >= 18)
     -- age must be >= 18
 );
+
 CREATE TABLE process (
-    ssn VARCHAR(20),
-    order_no INT,
-    CONSTRAINT pk_process PRIMARY KEY(ssn, order_no),
-    CONSTRAINT fk_process_employee FOREIGN KEY(ssn)
-        REFERENCES employee(ssn),
-    CONSTRAINT fk_process_orders FOREIGN KEY(order_no)
-        REFERENCES orders(order_no)
+    ssn VARCHAR(20) REFERENCES employee,
+    order_no INT REFERENCES orders,
+    PRIMARY KEY (ssn, order_no)
 );
+
 CREATE TABLE department (
-    name VARCHAR,
-    CONSTRAINT pk_department PRIMARY KEY(name)
+    name VARCHAR PRIMARY KEY
 );
+
 CREATE TABLE workplace (
-    address VARCHAR,
-    lat NUMERIC(8,6) NOT NULL,
-    long NUMERIC(9,6) NOT NULL,
+    address VARCHAR PRIMARY KEY,
+    lat NUMERIC(8, 6) NOT NULL,
+    long NUMERIC(9, 6) NOT NULL,
     UNIQUE(lat, long),
-    CONSTRAINT pk_workplace PRIMARY KEY(address),
     CHECK (lat >= -90 AND lat <= 90),
     CHECK (long >= -180 AND long <= 180)
     -- address must be in warehouse or office but not both
 );
+
 CREATE TABLE office (
-    address VARCHAR(255),
-    CONSTRAINT pk_office PRIMARY KEY(address),
-    CONSTRAINT fk_office_workplace FOREIGN KEY(address)
-        REFERENCES workplace(address)
+    address VARCHAR(255) PRIMARY KEY REFERENCES workplace
 );
+
 CREATE TABLE warehouse (
-    address VARCHAR(255),
-    CONSTRAINT pk_warehouse PRIMARY KEY(address),
-    CONSTRAINT fk_warehouse_workplace FOREIGN KEY(address)
-        REFERENCES workplace(address)
+    address VARCHAR(255) PRIMARY KEY REFERENCES workplace
 );
+
 CREATE TABLE works (
-    ssn VARCHAR(20),
-    name VARCHAR(200),
-    address VARCHAR(255),
-    CONSTRAINT pk_works PRIMARY KEY(ssn, name, address),
-    CONSTRAINT fk_works_employee FOREIGN KEY(ssn)
-        REFERENCES employee(ssn),
-    CONSTRAINT fk_works_department FOREIGN KEY(name)
-        REFERENCES department(name),
-    CONSTRAINT fk_works_workplace FOREIGN KEY(address)
-        REFERENCES workplace(address)
+    ssn VARCHAR(20) REFERENCES employee,
+    name VARCHAR(200) REFERENCES department,
+    address VARCHAR(255) REFERENCES workplace,
+    PRIMARY KEY (ssn, name, address)
 );
+
 CREATE TABLE product (
-    sku VARCHAR(25),
+    sku VARCHAR(25) PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     description VARCHAR,
-    price NUMERIC(10,2) NOT NULL,
-    ean NUMERIC(13) UNIQUE,
-    CONSTRAINT pk_product PRIMARY KEY(sku),
-    CHECK (price >= 0)
+    price NUMERIC(10, 2) NOT NULL,
+    ean NUMERIC(13) UNIQUE
 );
+
 CREATE TABLE contains (
-    order_no INT,
-    sku VARCHAR(25),
+    order_no INT REFERENCES orders,
+    sku VARCHAR(25) REFERENCES product,
     qty INT NOT NULL,
-    CONSTRAINT pk_contains PRIMARY KEY(order_no, sku),
-    CONSTRAINT fk_contains_orders FOREIGN KEY(order_no)
-        REFERENCES orders(order_no),
-    CONSTRAINT fk_contains_product FOREIGN KEY(sku)
-        REFERENCES product(sku),
+    PRIMARY KEY (order_no, sku),
     CHECK (qty > 0)
 );
+
 CREATE TABLE supplier (
-    tin VARCHAR(20),
+    tin VARCHAR(20) PRIMARY KEY,
     name VARCHAR(200),
     address VARCHAR(255),
-    sku VARCHAR(25),
-    date DATE,
-    CONSTRAINT pk_supplier PRIMARY KEY(tin),
-    CONSTRAINT fk_supplier_product FOREIGN KEY(sku)
-        REFERENCES product(sku)
+    sku VARCHAR(25) REFERENCES product,
+    date DATE
 );
+
 CREATE TABLE delivery (
-    address VARCHAR(255),
-    tin VARCHAR(20),
-    CONSTRAINT pk_delivery PRIMARY KEY(address, tin),
-    CONSTRAINT fk_delivery_warehouse FOREIGN KEY(address)
-        REFERENCES warehouse(address),
-    CONSTRAINT fk_delivery_supplier FOREIGN KEY(tin)
-        REFERENCES supplier(tin)
+    address VARCHAR(255) REFERENCES warehouse,
+    tin VARCHAR(20) REFERENCES supplier,
+    PRIMARY KEY (address, tin)
 );
