09f767982b333ea20ad1f0779b7b1f86d16029fe
vascopaisana <vasco.paisana3@gmail.com>
2023-06-07 19:54:45 +0100

    OLAP queries done

--- scripts/analytics.sql
+++ scripts/analytics.sql
@@ -10,6 +10,21 @@
 
 -- (1): Analyze the total sales quantity and values of each product in 2022,  globally, by city, by
 --      month, by day of the month, and by day of the week.
+SELECT sku, city, month, day_of_month, day_of_week,  SUM(qty) AS qty, SUM(total_price) AS total_price
+FROM product_sales
+WHERE MAKE_DATE(year, month, day_of_month)
+    BETWEEN '2022-01-01' AND '2023-12-31'
+GROUP BY GROUPING SETS(sku, (sku, city), (sku, city, month, day_of_month, day_of_week))
+ORDER BY sku NULLS FIRST, day_of_month NULLS FIRST, month NULLS FIRST, city NULLS FIRST;
 
 -- (2): Analyze the average daily sales value for all products in 2022, globally, by month and day of the
 --      week.
+SELECT month, day_of_week, AVG(daily_price) AS average_daily_sales
+FROM
+(SELECT month, day_of_week, day_of_month, SUM(total_price) AS daily_price
+FROM  product_sales
+WHERE MAKE_DATE(year, month, day_of_month)
+    BETWEEN '2022-01-01' AND '2023-12-31'
+GROUP BY (day_of_month, month, day_of_week)) as daily_product_sales
+GROUP BY GROUPING SETS((day_of_week, day_of_month, month), ())
+ORDER BY month NULLS FIRST, day_of_week NULLS FIRST;
