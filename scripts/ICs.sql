485e75115d8f41c444e8bbbc36a22ed536de0238
Raquel <raquelbb28.08@gmail.com>
2023-06-08 17:04:29 +0100

    Add Integrity constraints

--- scripts/ICs.sql
+++ scripts/ICs.sql
@@ -11,14 +11,43 @@
 ----------
 -- (IC-1): An `employee` must be 18 or older.
 ----------
-
-ALTER TABLE employee ADD CONSTRAINT CHECK_employee_age
+ALTER TABLE employee ADD CONSTRAINT check_employee_age
 CHECK (EXTRACT(YEAR FROM AGE(bdate)) >= 18);
 
 ----------
 -- (IC-2): A `workplace` is necessarily an `office` or `warehouse` but cannot be both.
 ----------
+CREATE OR REPLACE FUNCTION workplace_is_not_valid()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (
+        EXISTS (SELECT 1 FROM office WHERE address = NEW.address) AND
+        EXISTS (SELECT 1 FROM warehouse WHERE address = NEW.address)
+        ) OR (
+        NOT EXISTS (SELECT 1 FROM office WHERE address = NEW.address) AND
+        NOT EXISTS (SELECT 1 FROM warehouse WHERE address = NEW.address)
+        ) THEN
+            RAISE EXCEPTION 'Workplace located at %, is not valid.', NEW.address;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER worplace_is_not_valid_trigger AFTER INSERT OR UPDATE ON workplace
+FOR EACH ROW EXECUTE FUNCTION workplace_is_not_valid();
 
 ----------
 -- (IC-3): An `order` must be in `contains`.
 ----------
+CREATE OR REPLACE FUNCTION order_not_in_contains() RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.order_no NOT IN (SELECT order_no FROM orders) THEN
+        RAISE EXCEPTION 'Order % does not exist and can''t be associated with any products.', NEW.order_no;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER order_not_in_contains_trigger
+AFTER INSERT OR UPDATE ON contains
+FOR EACH ROW EXECUTE FUNCTION order_not_in_contains();
