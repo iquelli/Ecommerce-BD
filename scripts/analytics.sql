06136f613d229315d25de64d492b0809f5337121
Gocho1234 <goncalo.barias@gmail.com>
2023-06-12 16:04:22 +0100

    Refactor OLAPS one more time

--- scripts/analytics.sql
+++ scripts/analytics.sql
@@ -8,40 +8,24 @@
 --		Description: Uses the view developed for question 4 to write two OLAP
 --                   queries that lets us analyze information from the tables.
 
--- (1): Analyze the total sales quantity and values of each product in 2022,  globally, by city, by month,
--- day of the month and day of the week.
-WITH p_c_d AS (
-  SELECT
-    CAST(TO_CHAR(dates, 'DD') AS INT) AS day_of_month,
-    CAST(TO_CHAR(dates, 'MM') AS INT) AS month,
-    CAST(TO_CHAR(dates, 'YYYY') AS INT) AS year,
-    TO_CHAR(dates, 'Day') AS day_of_week,
-    skus.sku,  
-    cities.city
-  FROM GENERATE_SERIES('2022-01-01'::DATE, '2022-12-31'::DATE, '1 day') AS dates 
-    CROSS JOIN 
-      (SELECT DISTINCT sku FROM product_sales) AS skus,
-      (SELECT DISTINCT city FROM product_sales) AS cities 
+-- (1): Analyze the total sales quantity and values of each product in 2022, globally, by city, by month,
+--      day of month and day of week.
+WITH dpc AS (
+    SELECT EXTRACT(YEAR FROM dates) AS year,
+        EXTRACT(MONTH FROM dates) AS month,
+        EXTRACT(DAY FROM dates) AS day_of_month,
+        EXTRACT(DOW FROM dates) AS day_of_week, sku, city
+    FROM GENERATE_SERIES('2022-01-01'::DATE, '2022-12-31'::DATE, '1 day') AS dates
+        CROSS JOIN (SELECT DISTINCT sku FROM product_sales) AS skus,
+            (SELECT DISTINCT city FROM product_sales) AS cities
 )
-SELECT
-  p_c_d.SKU, p_c_d.city, p_c_d.month, p_c_d.day_of_month, p_c_d.day_of_week,
-  SUM(COALESCE(qty, 0)) AS qty,
-  SUM(COALESCE(total_price, 0)) AS total_price
+SELECT dpc.SKU, dpc.city, dpc.month, dpc.day_of_month, dpc.day_of_week,
+    SUM(COALESCE(qty, 0)) AS t_qty, SUM(COALESCE(total_price, 0)) AS t_value
 FROM product_sales
-  RIGHT JOIN p_c_d ON (
-      p_c_d.day_of_month = product_sales.day_of_month
-      AND p_c_d.month = product_sales.month
-      AND p_c_d.year = product_sales.year
-      AND p_c_d.sku = product_sales.sku
-      AND p_c_d.city = product_sales.city
-  )
-GROUP BY 
-  GROUPING SETS (p_c_d.sku, (p_c_d.sku, p_c_d.city)), 
-  GROUPING SETS ((), p_c_d.month, p_c_d.day_of_month, p_c_d.day_of_week)
-ORDER BY
-  p_c_d.SKU, p_c_d.city, p_c_d.month, p_c_d.day_of_month, p_c_d.day_of_week;
-
-
+    RIGHT JOIN dpc USING (year, month, day_of_month, sku, city)
+GROUP BY GROUPING SETS (dpc.SKU, (dpc.SKU, dpc.city, dpc.month),
+    (dpc.SKU, dpc.city, dpc.day_of_month), (dpc.SKU, dpc.city, dpc.day_of_week))
+ORDER BY SKU, city, month, day_of_month, day_of_week;
 
 -- (2): Analyze the average daily sales value for all products in 2022, globally, by month and day of the
 --      week.
@@ -58,7 +42,6 @@ WITH daily_sales AS (
 )
 SELECT dates.month, dates.day_of_week, AVG(COALESCE(daily_values, 0)) AS average_sales_2022
 FROM daily_sales
-    RIGHT JOIN dates ON (dates.month = daily_sales.month
-        AND dates.day_of_month = daily_sales.day_of_month)
+    RIGHT JOIN dates USING (month, day_of_month)
 GROUP BY GROUPING SETS ((), dates.month, dates.day_of_week)
 ORDER BY month, day_of_week;
