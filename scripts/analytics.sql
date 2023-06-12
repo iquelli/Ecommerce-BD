545d581a0773074c90a127037a3663ed1d31a592
vascopaisana <vasco.paisana3@gmail.com>
2023-06-12 13:01:15 +0100

    Update analytics.sql

--- scripts/analytics.sql
+++ scripts/analytics.sql
@@ -10,46 +10,38 @@
 
 -- (1): Analyze the total sales quantity and values of each product in 2022,  globally, by city, by month,
 -- day of the month and day of the week.
-WITH all_comb AS (
+WITH p_c_d AS (
   SELECT
     CAST(TO_CHAR(dates, 'DD') AS INT) AS day_of_month,
     CAST(TO_CHAR(dates, 'MM') AS INT) AS month,
     CAST(TO_CHAR(dates, 'YYYY') AS INT) AS year,
     TO_CHAR(dates, 'Day') AS day_of_week,
-    sku.sku,
-    city.city
-  FROM GENERATE_SERIES('2022-01-01'::DATE, '2022-12-31'::DATE, '1 day') AS dates
-  CROSS JOIN (SELECT DISTINCT sku FROM product_sales) AS sku,
-  (SELECT DISTINCT city FROM product_sales) as city
+    skus.sku,  
+    cities.city
+  FROM GENERATE_SERIES('2022-01-01'::DATE, '2022-12-31'::DATE, '1 day') AS dates 
+    CROSS JOIN 
+      (SELECT DISTINCT sku FROM product_sales) AS skus,
+      (SELECT DISTINCT city FROM product_sales) AS cities 
 )
 SELECT
-  all_comb.SKU,
-  all_comb.city,
-  all_comb.month,
-  all_comb.day_of_month,
-  all_comb.day_of_week,
+  p_c_d.SKU, p_c_d.city, p_c_d.month, p_c_d.day_of_month, p_c_d.day_of_week,
   SUM(COALESCE(qty, 0)) AS qty,
   SUM(COALESCE(total_price, 0)) AS total_price
 FROM product_sales
-  RIGHT JOIN all_comb ON (
-      all_comb.day_of_month = product_sales.day_of_month
-      AND all_comb.month = product_sales.month
-      AND all_comb.year = product_sales.year
-      AND all_comb.sku = product_sales.sku
-      AND all_comb.city = product_sales.city
-      )
-GROUP BY GROUPING SETS (
-    all_comb.SKU,
-    (all_comb.SKU, all_comb.city, all_comb.month),
-    (all_comb.SKU, all_comb.city, all_comb.day_of_month),
-    (all_comb.SKU, all_comb.city, all_comb.day_of_week)
-    )
+  RIGHT JOIN p_c_d ON (
+      p_c_d.day_of_month = product_sales.day_of_month
+      AND p_c_d.month = product_sales.month
+      AND p_c_d.year = product_sales.year
+      AND p_c_d.sku = product_sales.sku
+      AND p_c_d.city = product_sales.city
+  )
+GROUP BY 
+  GROUPING SETS (p_c_d.sku, (p_c_d.sku, p_c_d.city)), 
+  GROUPING SETS ((), p_c_d.month, p_c_d.day_of_month, p_c_d.day_of_week)
 ORDER BY
-  all_comb.SKU,
-  all_comb.city,
-  all_comb.day_of_week,
-  all_comb.day_of_month,
-  all_comb.month;
+  p_c_d.SKU, p_c_d.city, p_c_d.month, p_c_d.day_of_month, p_c_d.day_of_week;
+
+
 
 -- (2): Analyze the average daily sales value for all products in 2022, globally, by month and day of the
 --      week.
